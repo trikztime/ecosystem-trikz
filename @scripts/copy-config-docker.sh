323ca212bd36e7b85e190aea356bd8000515184e
not-log <56871060+not-log@users.noreply.github.com>
2023-03-22 23:24:54 +0400

    удалены лишние файлы

--- @scripts/copy-config-docker.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p ../services/api/src/config && cp ../@config/config.json $_/config.json
-mkdir -p ../services/gateway/src/config && cp ../@config/config.json $_/config.json
-mkdir -p ../services/skillrank/src/config && cp ../@config/config.json $_/config.json
