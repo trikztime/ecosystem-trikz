6d4abe7bc81dbe4e2283fd95772d4613ba6575e2
not-log <56871060+not-log@users.noreply.github.com>
2023-08-03 21:47:04 +0400

    обновлены скрипты для работы на юникс системах (2)

--- @dev-scripts/link-lib.sh
+++ @dev-scripts/link-lib.sh
@@ -1,17 +1,20 @@
-cd ../services/api
-npm link @trikztime/ecosystem-shared
+cd ../libs/ecosystem-shared/dist #
+npm link #
 
-cd ../discord
-npm link @trikztime/ecosystem-shared
+cd ../services/api #
+npm link @trikztime/ecosystem-shared #
 
-cd ../gateway
-npm link @trikztime/ecosystem-shared
+cd ../discord #
+npm link @trikztime/ecosystem-shared #
 
-cd ../gateway-socket
-npm link @trikztime/ecosystem-shared
+cd ../gateway #
+npm link @trikztime/ecosystem-shared #
 
-cd ../skillrank
-npm link @trikztime/ecosystem-shared
+cd ../gateway-socket #
+npm link @trikztime/ecosystem-shared #
 
-cd ../steam
-npm link @trikztime/ecosystem-shared
+cd ../skillrank #
+npm link @trikztime/ecosystem-shared #
+
+cd ../steam #
+npm link @trikztime/ecosystem-shared #
