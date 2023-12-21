e3144de0a2090a4b51c7e18deaed82a529c70f62
not-log <56871060+not-log@users.noreply.github.com>
2023-12-21 23:15:55 +0400

    открытие /api, закрытие /skillrank

--- services/gateway/src/main.ts
+++ services/gateway/src/main.ts
@@ -3,12 +3,10 @@ import { NestFactory } from "@nestjs/core";
 
 import { AppModule } from "./app.module";
 import { HttpExceptionFilter } from "./filters";
-import { HostGuard } from "./guards";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalGuards(new HostGuard());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
 
